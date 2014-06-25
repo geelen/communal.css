4f54167863f5286609b7349fd26a0316bb7c941f
Glen Maddern <glenmaddern@gmail.com>
2014-06-25 18:34:29 +1000

    got recoverable scss building working!

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins');
+"use strict";
 
-gulp.task('default', function () {
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  safe = function(plugin) {
+    return plugin.on('error', function (e) {
+      $.util.log(e);
+      this.emit('end');
+    })
+  };
+
+gulp.task('build', function () {
   return gulp.src('src/communal.scss')
-    .pipe($.sass())
-    .pipe($.autoprefixer())
+    .pipe(safe($.sass()))
+    .pipe($.autoprefixer('last 1 version'))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('watch', function () {
+  gulp.watch('src/**', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
